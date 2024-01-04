89ca7693432e2b921d9463ed4c9561943f6798a8
Alisson Youssf <youssfbr@gmail.com>
2024-01-04 14:48:49 -0300

    Custom validation response

---
+++ src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+    private static final long serialVersionUID = 1L;
+    private final List<FieldMessage> errors = new ArrayList<>();
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addErrors(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
