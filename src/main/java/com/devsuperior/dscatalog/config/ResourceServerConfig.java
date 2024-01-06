f70ef26d111f0b3eef12c734c078122c59e82db2
Alisson Youssf <youssfbr@gmail.com>
2024-01-06 16:41:43 -0300

    H2 security configuration

--- src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -8,17 +9,21 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private final JwtTokenStore tokenStore;
+    private final Environment env;
     private static final String[] ADMIN = { "/users/**" };
-    private static final String[] PUBLIC = { "/oauth/token" };
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
-    public ResourceServerConfig(JwtTokenStore tokenStore) {
+    public ResourceServerConfig(JwtTokenStore tokenStore , Environment env) {
         this.tokenStore = tokenStore;
+        this.env = env;
     }
 
     @Override
@@ -28,6 +33,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
